2453a7f3b0259d8d1b3dcf68cc3bc371f21e1179
David Wales <daviewales@disroot.org>
2022-08-08 22:34:40 +1000

    Update build_openssl_universal_osx.sh

--- build_openssl_universal_osx.sh
+++ build_openssl_universal_osx.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 OPENSSL_VERSION="1.1.1i"
-MACOSX_DEPLOYMENT_TARGET="10.13"
+
+export MACOSX_DEPLOYMENT_TARGET=10.13
+export MIN_SUPPORTED_MACOSX_DEPLOYMENT_TARGET=10.13
+export LDFLAGS='-mmacosx-version-min=10.13'
+export CFLAGS='-stdlib=libc++ -mmacosx-version-min=10.13'
+export CXXFLAGS='-stdlib=libc++ -mmacosx-version-min=10.13'
 
 curl -OL http://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz
 tar -xzvf openssl-$OPENSSL_VERSION.tar.gz
